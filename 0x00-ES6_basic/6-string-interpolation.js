43a5e3a3a6f176c3f21c70daf579d123cefb4c2b
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-04-23 01:51:11 +0100

    Take advantage of template literals

--- 0x00-ES6_basic/6-string-interpolation.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
-  };
-}
